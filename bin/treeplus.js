fceae67027dcb23f499ebcdb79b65a4a70f242c0
artiely <1119696785@qq.com>
2020-01-07 14:39:43 +0800

    更新，新增只打印文件夹和指定层级打印

--- bin/treeplus.js
+++ bin/treeplus.js
@@ -4,7 +4,11 @@ const argv = require('yargs')
   .usage('Usage: tp [options]')
   .example('tp -i node_modules', '排除node_modules文件夹')
   .alias('i', 'ignore')
+  .alias('d', 'dir')
+  .alias('l', 'lev')
   .describe('i', '您要排除显示的文件夹')
+  .describe('d', '只打印文件夹')
+  .describe('l', '指定打印的层级')
   .help('help').argv
 const fs = require('fs')
 const path = require('path')
@@ -28,8 +32,8 @@ function deep(target, depth, ignore) {
     for (var i = 0; i < ignore.length; i++) {
       for (var j = 0; j < dirs.length; j++) {
         if (dirs[j] == ignore[i]) {
-          dirs.splice(j, 1);
-          j--;
+          dirs.splice(j, 1)
+          j--
         }
       }
     }
@@ -37,14 +41,24 @@ function deep(target, depth, ignore) {
 
   dirs.forEach(dir => {
     console.log(`${prefix}├──${dir}`)
-    deep(path.join(target, dir), depth + 1)
+    // console.log('TCL: argv', argv.l, depth)
+    if (!argv.l) {
+      deep(path.join(target, dir), depth + 1)
+    } else {
+      if (argv.l > depth + 1) {
+        deep(path.join(target, dir), depth + 1)
+      }
+    }
   })
 
   var count = files.length - 1
-  files.forEach(file => {
-    console.log(`${prefix}${count-- ? '├─' : '└─'}─${file}`)
-  })
+  if (!argv.d || !argv.dir) {
+    files.forEach(file => {
+      console.log(`${prefix}${count-- ? '├─' : '└─'}─${file}`)
+    })
+  }
 }
+
 if (argv.ignore) {
   if (argv.ignore !== true) {
     var _ignore = argv._
